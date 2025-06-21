b5b438aa996799c3f5fa0bb12e69becc3c629909
Tewodros <tewodrosy21@gmail.com>
2025-06-21 12:55:02 +0300

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import express from "express";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
+
+if (process.env.NODE_ENV === "production") job.start();
 
 dotenv.config();
 const app = express();
@@ -10,6 +13,10 @@ app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "Ok" });
+});
 
 app.use("/api/transactions", transactionsRoute);
 initDB().then(() => {
